b42b5cacc3c3957d8020127205a132b5ac806cc2
Jaimemlz <jaimemlz26@gmail.com>
2024-10-12 14:46:48 +0200

    chore: format code with Prettier

--- src/styles/touch-the-mole.styles.css.js
+++ src/styles/touch-the-mole.styles.css.js
@@ -1,12 +1,13 @@
-import { css } from 'lit';
+import { css } from "lit";
 
 export const touchTheMoleStyles = css`
   :host {
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
     font-size: 2.5em;
     color: #333;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
   }
 `;
--- src/touch-the-mole.view.js
+++ src/touch-the-mole.view.js
@@ -1,15 +1,15 @@
-import { css, html } from 'lit'
-import { TouchTheMoleViewmodel } from "./touch-the-mole.viewmodel"
-import { touchTheMoleStyles } from './styles/touch-the-mole.styles.css';
+import { html } from "lit";
+import { TouchTheMoleViewmodel } from "./touch-the-mole.viewmodel";
+import { touchTheMoleStyles } from "./styles/touch-the-mole.styles.css";
+import "./components/game-panel/game-panel.view";
 
 export class TouchTheMoleView extends TouchTheMoleViewmodel {
   static styles = [touchTheMoleStyles];
 
   render() {
-    return html`
-      <p>TOCA AL TOPO</p>
-    `
+    return html` <p>TOCA AL TOPO</p>
+      <game-panel></game-panel>`;
   }
 }
 
-window.customElements.define('touch-the-mole', TouchTheMoleView)
+window.customElements.define("touch-the-mole", TouchTheMoleView);
