48b30a43d1dbda2bba464a71a4d1936722b366ec
Jaimemlz <jaimemlz26@gmail.com>
2024-10-13 18:50:24 +0200

    fix: update base URL in vite.config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: 'https://jaimemlz.github.io/touch-the-mole/',
+  base: "/touch-the-mole/",
 });
