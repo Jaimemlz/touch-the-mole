1d9300c921c97d941e37ae77d0800eb200b20296
Jaimemlz <jaimemlz26@gmail.com>
2024-10-15 19:30:13 +0200

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://jaimemlz.github.io/touch-the-mole/sw.js', { scope: 'https://jaimemlz.github.io/touch-the-mole/' })})}
